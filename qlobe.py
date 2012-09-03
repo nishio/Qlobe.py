1f557f064d3bd3deda3860f114f3c18bef57c30f
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-09-03 21:29:09 +0900

    working

--- qlobe.py
+++ qlobe.py
@@ -1,3 +1,4 @@
+v=0000;s="""
 d="^Lcf<LK8,_@7gj*LJ=c5nM)Tp1g0%Xv.,S[<>YoP4ZojjV)O>qIH1/n[|2yE[>:ieC97N-A&Kj_K_><wS5rtWk@*a+Y5yH?b[F^e7C/56j|pmRe+:)BO98(Zh)'Iof*nm.,$C5Nyt=PPu01Avw^<IiQ=5$'D-y?g6`YT+qLw9k^ch|K'),tc6ygIL8xI#LNz3v}T=4WlL27FZ0ij)7TQCI)P7u}RT5-iJbbG5P-DHB<.R,YvZ_rnv6ky-G+4U'$*are@b4U351Q-ug500x8RR%`Om7VDp4M5PFixrPvl&<p[]1IJEGgDt8Lm#;bc4zS^y]0`_PstfUxOC(q/,}.YOIFj(k&q_VzcaAi?]^lCVYp";
 
 import zlib, struct, math, sys
@@ -13,7 +14,7 @@ n=reduce(lambda x, y: x * 90 + (y - 2) % 91, d, 0)
 e=zlib.decompress(("%x" % n).decode('hex'))
 # TODO
 e="".join("{0:08b}".format(ord(byte)) for byte in e)
-result = [" "] * (80 * 25)
+result = list(s.ljust(80 * 25))
 for y in range(22):
     w = int((math.sqrt(1-((y*2.0-21)/22)**(2))*23));
     for x in range(w * 2 - 1):
@@ -22,20 +23,10 @@ for y in range(22):
         start = 90 * x / w + v + 90
         u = u[start : start + 90 / w];
         result[(y*80)+120-w+x] = " .:%#"[4 * u.count("0") / len(u)]
-        #s[(y*80)+120-w+x]=(""<<32<<".:%#")[4*u.count(("0"))/u.size];
 
 for y in range(25):
     print "".join(result[y*80:y*80+80])
 
-"""
-22.times{|y|
-  w=(Math.sqrt(1-((y*2.0-21)/22)**(2))*23).floor;
-  (w*2-1).times{
-    |x|u=(e)[y*z=360,z]*2;
-     u=u[90*x/w+v+90,90/w];
-     s[(y*80)+120-w+x]=(""<<32<<".:%#")[4*u.count(("0"))/u.size];
-   }
-};;
-  puts s+"; _ The Qlobe#{" "*18+ ("Copyright(C).Yusuke Endoh, 2010")}";
-  exit;
-"""
+#  puts s+"; _ The Qlobe#{" "*18+ ("Copyright(C).Yusuke Endoh, 2010")}";
+#  exit;
+""";exec(s)
